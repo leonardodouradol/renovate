08b2d63621a8d3a4e2c7256b5f968aab151e988d
Leonardo Dourado <leonardo.dourado@useblu.com.br>
2025-02-06 10:18:21 -0300

    fix: remove key

--- src/teste.ts
+++ src/teste.ts
@@ -4,4 +4,3 @@ const cacheKey = (uuid: string) => `navigation-menu-cache-v${version}`;
 const cacheExpireKey = `navigation-menu-cache-expire-v${version}`;
 const cacheRevalidateKey = `navigation-menu-cache-revalidate-v${version}`;
 const cacheRevalidateTime = 1000 * 60 * 5; // 5 minutes
-const cacheExpireTime = 1000 * 60 * 60 * 24; // 1 day

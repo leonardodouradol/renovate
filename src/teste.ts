77327442f1d921d021926243f547277d3d610f7b
Leonardo Dourado <leonardo.dourado@useblu.com.br>
2025-02-06 10:17:06 -0300

    chore: add workflow to update cache version

---
+++ src/teste.ts
@@ -0,0 +1,7 @@
+const version = 1;
+const globalKey = `navigation-menu-cache-v${version}`;
+const cacheKey = (uuid: string) => `navigation-menu-cache-v${version}`;
+const cacheExpireKey = `navigation-menu-cache-expire-v${version}`;
+const cacheRevalidateKey = `navigation-menu-cache-revalidate-v${version}`;
+const cacheRevalidateTime = 1000 * 60 * 5; // 5 minutes
+const cacheExpireTime = 1000 * 60 * 60 * 24; // 1 day
